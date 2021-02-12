301b5b86af79d55c5b43dfd23587ead1f1cdf165
Ivan Sapronov <sapronov_ivan@mail.ru>
2021-02-12 22:15:06 +0300

    feat: Добавлены страницы администратора и пользователя и методы контроллера к ним. Доработан LoginSuccessHandler согласно условию задачи.

--- src/main/java/ru/sapronov/springsecurity/controller/UserController.java
+++ src/main/java/ru/sapronov/springsecurity/controller/UserController.java
@@ -1,9 +1,14 @@
 package ru.sapronov.springsecurity.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ru.sapronov.springsecurity.models.User;
+import ru.sapronov.springsecurity.service.UserService;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +16,12 @@ import java.util.List;
 @Controller
 @RequestMapping("/")
 public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+//	@Autowired
+//	private RoleService roleService;
 
 	@RequestMapping(value = "hello", method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
@@ -22,9 +33,23 @@ public class UserController {
 		return "hello";
 	}
 
-    @RequestMapping(value = "login", method = RequestMethod.GET)
+	@GetMapping("login")
     public String loginPage() {
         return "login";
     }
+
+	@GetMapping("user")
+	public String userPage(Authentication authentication, ModelMap model) {
+		User user = userService.getUserByUsername(authentication.getName());
+		model.addAttribute("user", user);
+		return "user";
+	}
+
+	@GetMapping("/admin/index")
+	public String adminPage(ModelMap model) {
+		List<User> users = userService.index();
+		model.addAttribute("users", users);
+		return "index";
+	}
 
 }
